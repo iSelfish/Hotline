333b2000db11c2e019a5d2fcbba27dd2adb78796
dmytro_kolos <dmytro_kolos@epam.com>
2020-01-21 14:05:51 +0200

    refactored countExpensiveItems

--- src/test/java/ua/hotline/PageObjects/ItemList.java
+++ src/test/java/ua/hotline/PageObjects/ItemList.java
@@ -27,70 +27,31 @@ public class ItemList {
         return new ItemList();
     }
 
-    /*private int countExpensiveItemsOnCurrentPage(int maxPrice) {
-        int expensiveItemsCount = 0;
+    public int countExpensiveItems(int maxPrice, int amountOfPages) {
+        int page = 1;
         String priceString;
-        String checkClass;
-        String checkPrice;
         int price;
-        for (int i = 1; i < 21; i++) {
-            try {
-                checkClass = Driver.getDriver().findElement(By.xpath("//li[@class='product-item'][" + i + "]/div[3]")).getAttribute("class");
-                System.out.println("Item = " + i);
-                if (checkClass.equals("item-price stick-bottom")) {
-                    checkPrice = Driver.getDriver().findElement(By.xpath("//li[@class='product-item'][" + i + "]/div[3]/div[@class='stick-pull cell-xs-6'][1]/div[@class='price-md']/span[1]")).getAttribute("class");
-                    if (checkPrice.equals("price-format")) {
-                        priceString = Driver.getDriver().findElement(By.xpath("//li[@class='product-item'][" + i + "]/div[@class='item-price stick-bottom']/div[@class='stick-pull cell-xs-6'][1]/div[@class='price-md']/span[@class='price-format']/span[@class='value']")).getText();
-                    } else {
-                        priceString = Driver.getDriver().findElement(By.xpath("//li[@class='product-item'][" + i + "]/div[@class='item-price stick-bottom']/div[@class='stick-pull cell-xs-6'][1]/div[@class='price-md']/span[@class='value']")).getText();
-                    }
-                    priceString = priceString.replaceAll(" ", "");
-                    price = Integer.parseInt(priceString);
-                    System.out.println("price = " + price);
-                    if (price > maxPrice) {
-                        expensiveItemsCount++;
-                    }
-                }
-            } catch (NoSuchElementException ignored) {
-            }
-        }
-        return expensiveItemsCount;
-    }*/
-    private int countExpensiveItemsOnCurrentPage(int maxPrice) {
-        int expensiveItemsCount = 0;
-        String priceString;
-        String checkClass;
-        int price;
-        for (int i = 1; i < 21; i++) {
-            try {
-                checkClass = Driver.getDriver().findElement(By.xpath("//li[@class='product-item'][" + i + "]/div[3]")).getAttribute("class");
-                System.out.println("Item = " + i);
-                if (checkClass.equals("item-price stick-bottom")) {
+        int expensiveItemsCounter = 0;
+        do {
+            System.out.println("current page = " + page);
+            for (int i = 1; i < 21; i++) {
+                try {
+                    System.out.println("Item = " + i);
                     priceString = Driver.getDriver().findElement(By.xpath("//li[@class='product-item'][" + i + "]//span[@class='value']")).getText();
                     priceString = priceString.replaceAll(" ", "");
                     price = Integer.parseInt(priceString);
                     System.out.println("price = " + price);
                     if (price > maxPrice) {
-                        expensiveItemsCount++;
+                        expensiveItemsCounter++;
                     }
+                } catch (NoSuchElementException ignored) {
                 }
-            } catch (NoSuchElementException ignored) {
             }
-        }
-        return expensiveItemsCount;
-    }
-
-    public int countExpensiveItems(int maxPrice, int amountOfPages) {
-        int page = 1;
-        int expansiveItemsCounter = 0;
-        do {
-            System.out.println("current page = " + page);
-            expansiveItemsCounter += new ItemList().countExpensiveItemsOnCurrentPage(maxPrice);
-            System.out.println("expensiveItemsCounter = " + expansiveItemsCounter);
+            System.out.println("expensiveItemsCounter = " + expensiveItemsCounter);
             new ItemList().nextPage();
             page++;
         } while (page < (amountOfPages + 1));
-        return expansiveItemsCounter;
+        return expensiveItemsCounter;
     }
 
     private ItemList nextPage() {
